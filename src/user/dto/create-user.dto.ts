88b1f4a9adb028502704443bf63356927f470a9b
iwin2471 <iwin12372@gmail.com>
2023-11-02 19:59:46 +0900

    [add] user auth

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
+
+const passwordRegEx =   /^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,20}$/;
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(2, { message: "name must have 2" })
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  @Matches(passwordRegEx, {
+    message: `Password must contain Minimum 8 and maximum 20 characters,
+    at least one uppercase letter,
+    one lowercase letter,
+    one number and
+    one special character`,
+  })
+  password: string;
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from "./entities/user.entity";
 
 @Injectable()
 export class UserService {
+  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {
+  }
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user: User = new User();
+    user.name = createUserDto.name;
+    user.password = createUserDto.password;
+    return this.userRepository.save(user);
   }
 
-  findAll() {
-    return `This action returns all user`;
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: number): Promise<User> {
+    return this.userRepository.findOneBy({id});
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
